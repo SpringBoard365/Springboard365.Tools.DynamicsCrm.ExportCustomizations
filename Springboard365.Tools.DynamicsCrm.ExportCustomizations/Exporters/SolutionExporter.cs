5bc3fc4beacb8c1836d2d82f71fb9c208936efcc
David Small <dave@dsmall.co.uk>
2020-10-04 11:09:05 +0100

    feat: Increase Max Connection Timeout

--- Springboard365.Tools.DynamicsCrm.ExportCustomizations/Exporters/SolutionExporter.cs
+++ Springboard365.Tools.DynamicsCrm.ExportCustomizations/Exporters/SolutionExporter.cs
@@ -3,6 +3,7 @@
     using System;
     using Microsoft.Crm.Sdk.Messages;
     using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Tooling.Connector;
 
     public class SolutionExporter : ISolutionExporter
     {
@@ -15,6 +16,7 @@
 
         public byte[] ExportSolutionFile(string solutionName, bool exportManagedSolution)
         {
+            CrmServiceClient.MaxConnectionTimeout = new TimeSpan(0, 10, 0);
             var exportSolutionRequest = new ExportSolutionRequest
             {
                 Managed = exportManagedSolution,
