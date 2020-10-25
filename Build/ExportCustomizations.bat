ddbd2a36343f34ea245e5976a6fcdc2f3e373479
David Small <dave@dsmall.co.uk>
2020-10-25 11:20:01 +0000

    feat: Release v2.0.0 following testing.

--- Build/ExportCustomizations.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-IF NOT EXIST Springboard365.Tools.DynamicsCrm.ExportCustomizations\tools\ExportCustomizations.exe (
-  nuget install Springboard365.Tools.DynamicsCrm.ExportCustomizations -x -PreRelease 
-)
-
-Springboard365.Tools.DynamicsCrm.ExportCustomizations\tools\ExportCustomizations.exe
-
-pause
--- Build/NuGetPush.bat
+++ Build/NuGetPush.bat
@@ -1,5 +1,5 @@
 @echo off
-SET packageVersion=2.0.0-beta06
+SET packageVersion=2.0.0
 
 SET configuration=Release
 SET id=Springboard365.Tools.DynamicsCrm.ExportCustomizations
